5fdd2864835cbebaaf8fd49e0b96fc727ec0a374
filecc <fil.gia@live.it>
2023-06-09 11:38:37 +0200

    texture seeder and model

---
+++ database/seeders/TexturesSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Texture;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TexturesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $textures = config('dataSeeder.textures');
+        foreach ($textures as $key => $texture) {
+            $newTexture = new Texture();
+            $newTexture->name = $texture[$key];
+            $newTexture->slug = str_replace(' ', '-', $texture[$key]);
+            $newTexture->save();
+        }
+    }
+}
