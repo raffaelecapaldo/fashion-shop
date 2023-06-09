c80254efdbb47442c9ff0ea494c849c1a6cef708
totarochristian <totaro.christian.18@gmail.com>
2023-06-09 12:14:59 +0200

    Update the method create in brand controller

--- app/Http/Controllers/Admin/BrandController.php
+++ app/Http/Controllers/Admin/BrandController.php
@@ -24,12 +24,10 @@ class BrandController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        return view('admin.brands.create');
     }
 
     /**
