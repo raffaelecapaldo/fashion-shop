c7340d81c3b2eb9d121233f73bd058a0373b80ab
totarochristian <totaro.christian.18@gmail.com>
2023-06-09 11:32:28 +0200

    Define brands seeder

--- database/seeders/BrandSeeder.php
+++ database/seeders/BrandSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Brand;
+use Illuminate\Support\Str;
+
 class BrandSeeder extends Seeder
 {
     /**
@@ -14,6 +17,14 @@ class BrandSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $brands = config('dataSeeder.brands');
+        foreach ($brands as $brand) {
+            $new = new Brand();
+            $new->title = $brand['title'];
+            $new->slug = Str::slug($brand['title'], '-');
+            $new->image = $brand['image'];
+            $new->body = $brand['body'];
+            $new->save();
+        }
     }
 }
