92d9c42e668d700cd231b9f119899c22a5126432
totarochristian <totaro.christian.18@gmail.com>
2023-06-09 13:10:48 +0200

    Update the brands show page

--- resources/views/admin/brands/show.blade.php
+++ resources/views/admin/brands/show.blade.php
@@ -1,5 +1,11 @@
+
 @extends('layouts.admin.general')
+@section('title', 'Brands List')
+
 @section('content')
-    <h1>{{ $brand->name }}</h1>
+<div class="main-content">
     <img class="img-thumbnail" style="width:100px" src="{{ $brand->logo }}" alt="{{ $brand->name }}">
+    <h1>{{ $brand->name }}</h1>
+</div>
+
 @endsection
