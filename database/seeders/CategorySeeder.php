bd8b16073715d22813a866f461f8c74ee11c9ff8
nicolozibra1 <nico.zibra@gmail.com>
2023-06-09 12:02:17 +0200

    fix-category seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use Illuminate\Support\Str;
 
 class CategorySeeder extends Seeder
 {
