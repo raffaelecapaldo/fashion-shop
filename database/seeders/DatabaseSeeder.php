9d0ce9b6b609dfa9d405d7a34631aad016944698
filecc <fil.gia@live.it>
2023-06-09 12:20:58 +0200

    database seedrer

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
             BrandSeeder::class,
             CategorySeeder::class,
             ColorSeeder::class,
-            TexturesSeeder::class,
+            TextureSeeder::class,
             ProductSeeder::class
         ]);
     }
