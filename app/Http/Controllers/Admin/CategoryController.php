ca563785d3e93a0bd22a8a71771cc72e620e59fb
nicolozibra1 <nico.zibra@gmail.com>
2023-06-09 12:51:14 +0200

    fix-controller-category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -22,7 +22,7 @@ class CategoryController extends Controller
     public function index()
     {
         $categories = Category::all();
-        return view('admin.posts.index', compact('categories'));
+        return view('admin.categories.index', compact('categories'));
     }
 
     /**
