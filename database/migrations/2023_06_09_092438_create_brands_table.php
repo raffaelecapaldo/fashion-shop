6a0200391f5d60026ada89ac90545ed70422ee15
totarochristian <totaro.christian.18@gmail.com>
2023-06-09 11:28:06 +0200

    Define the migration to create brands table

--- database/migrations/2023_06_09_092438_create_brands_table.php
+++ database/migrations/2023_06_09_092438_create_brands_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
+            $table->string("name",100)->unique();
+            $table->string("slug");
+            $table->string("logo")->nullable();
             $table->timestamps();
         });
     }
