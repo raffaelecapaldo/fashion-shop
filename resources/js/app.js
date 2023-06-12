7949c159c1c45f84b06125ce47ae344b2f208752
totarochristian <totaro.christian.18@gmail.com>
2023-06-12 10:43:42 +0200

    Manage modal-delete

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,42 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+
+// delete modal
+const deleteSubmitButtons = document.querySelectorAll('.delete-button');
+
+deleteSubmitButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        const dataTitle = button.getAttribute('data-item-title');
+
+        const modal = document.getElementById('deleteModal');
+
+        const bootstrapModal = new bootstrap.Modal(modal);
+        bootstrapModal.show();
+
+        const modalItemTitle = modal.querySelector('#modal-item-title');
+        modalItemTitle.textContent = dataTitle;
+
+        const buttonDelete = modal.querySelector('button.btn-primary');
+
+        buttonDelete.addEventListener('click', () => {
+            button.parentElement.submit();
+        });
+    });
+});
+
+// image preview
+const previewImage = document.getElementById('image');
+previewImage.addEventListener('change', (event) => {
+    var oFReader = new FileReader();
+    // var image  =  previewImage.files[0];
+    // console.log(image);
+    oFReader.readAsDataURL(previewImage.files[0]);
+
+    oFReader.onload = function (oFREvent) {
+        //console.log(oFREvent);
+        document.getElementById("uploadPreview").src = oFREvent.target.result;
+    };
+});
