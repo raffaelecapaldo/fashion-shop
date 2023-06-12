4e8e0b30712d4c68c02857b86c889bbb187b85c7
nicolozibra1 <nico.zibra@gmail.com>
2023-06-12 11:20:24 +0200

    category create + upgrade index layout

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.admin.general')
 
 @section('content')
-
+<div class="main-content">
+    <h1 class="px-3 py-3">Add new Category</h1>
+    <form action="{{route('admin.categories.store')}}" method="POST">
+    @csrf
+        <div class="mb-3 px-3">
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror">
+            @error('name')
+                <div class="invalid-feedback">{{$message}}</div>
+            @enderror
+        </div>
+        <button type="submit" class="btn btn-success ms-3">Save</button>
+        <button type="reset" class="btn btn-danger ms-3">Reset</button>
+    </form>
+</div>
 @endsection
--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,41 +1,37 @@
 @extends('layouts.admin.general')
-@section('title', 'Categories List')
 
 @section('content')
 <div class="main-content">
-<div class="table-responsive">
-<table class="table">
-    <thead>
-        <tr>
-            <th scope="col" colspan="5" class="fs-3 text-center">Lista delle nostre categorie</th>
-        </tr>
-      <tr>
-        <th scope="col">Id</th>
-        <th scope="col">Nome</th>
-        <th scope="col">Data di creazione</th>
-        <th scope="col">Azioni</th>
-      </tr>
-    </thead>
-    <tbody>
-        @foreach ($categories as $category)
-            <tr>
-                <th scope="row">{{ $category->id }}</th>
-                <td>
-                    <a href="{{ route('admin.categories.show', $category->slug) }}" class="text-decoration-none fw-semibold">{{$category->name}}</a>
-                </td>
-                <td>{{ $category->created_at }}</td>
-                <td>
-                    <a href="">Edit</a>
-                    <a href="">Delete</a>
-                </td>
-            </tr>
+    <h1 class="mb-3 p-3 ms-4">Category List</h1>
+    <div class="text-end me-4 px-4">
+        <a class="btn btn-success" href="{{ route('admin.categories.create') }}">Add new category</a>
+    </div>
+    @if (session()->has('message'))
+        <div class="alert alert-success">
+            {{ session()->get('message') }}
+        </div>
+    @endif
+    <div class="row mt-3 px-5">
+        @foreach ($categories as $category )
+            <div class="col-3">
+                <div class="card mb-3 py-2">
+                    <div class="card-title text-center">
+                        <a href="{{ route('admin.categories.show', $category->slug) }}" class="text-decoration-none fw-semibold">
+                            {{$category->name}}
+                        </a>
+                    </div>
+                    <div class="card-body text-center d-flex justify-content-center gap-2">
+                        <a href="{{ route('admin.categories.edit', $category->slug) }}"><i class="fa-solid fa-pencil"></i></a>
+                        <form action="{{ route('admin.categories.destroy', $category->slug) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type='submit' class="delete-button" data-item-title="{{ $category->name }}"> <i
+                                    class="fa-solid fa-trash"></i></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         @endforeach
-    </tbody>
-  </table>
+    </div>
 </div>
-<div class="d-flex justify-content-end gap-2 me-2">
-    {{ $categories->links() }}
-</div>
-</div>
-
 @endsection
