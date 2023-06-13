aeb9735f76df7b276d1f2a0b7eeca03fac3363c6
totarochristian <totaro.christian.18@gmail.com>
2023-06-13 10:26:10 +0200

    Define the method index in the api product controller to return all the products (with brand and category data) paginated

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -9,5 +9,26 @@ use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    /**
+     * Return a json response with all the products saved in the local db
+     */
+    public function index()
+    {
+        //Eager: return also the brand and category data
+        $products = Product::with("brand","category")->paginate(5);
+
+        if($products){
+            return response()->json([
+                "success" => true,
+                "results" => $products
+            ]);
+        }else{
+            return response()->json([
+                "success" => false,
+                "results" => "Products not found!"
+            ]);
+        }
+    }
+
+
 }
