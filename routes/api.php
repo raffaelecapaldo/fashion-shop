b1a63db80784efe9e6c7885faa0f35bb2589b332
totarochristian <totaro.christian.18@gmail.com>
2023-06-13 10:30:56 +0200

    Update api routes to manage the category controller

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\BrandController;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,3 +26,5 @@ Route::get("/brands", [BrandController::class, 'index']);
 Route::get("/brands/{slug}", [BrandController::class, 'show']);
 Route::get("/products", [ProductController::class, 'index']);
 Route::get("/products/{slug}", [ProductController::class, 'show']);
+Route::get("/categories", [CategoryController::class, 'index']);
+Route::get("/categories/{slug}", [CategoryController::class, 'show']);
