5e4f118dbf351a9a5e1efbc7e086d7b2826865bf
totarochristian <totaro.christian.18@gmail.com>
2023-06-13 10:32:16 +0200

    Add method index in the api category controller to return all the categories

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -5,7 +5,27 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Category;
+
 class CategoryController extends Controller
 {
-    //
+    /**
+     * Return a json response with all the categories saved in the local db
+     */
+    public function index()
+    {
+        $categories = Category::paginate(5);
+
+        if($categories){
+            return response()->json([
+                "success" => true,
+                "results" => $categories
+            ]);
+        }else{
+            return response()->json([
+                "success" => false,
+                "results" => "Categories not found!"
+            ]);
+        }
+    }
 }
